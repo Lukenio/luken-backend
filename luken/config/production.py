df26d869534dcb8a6bcbbe19d50e12948bfbb2dc
Igor Polynets <igor.polynets2@coxautoinc.com>
2018-02-27 22:52:00 -0500

    fix email seding

--- luken/config/production.py
+++ luken/config/production.py
@@ -38,5 +38,5 @@ class Production(Common):
 
     ANYMAIL = {
         "MAILGUN_API_KEY": os.getenv('MAILGUN_API_KEY'),
-        "MAILGUN_SENDER_DOMAIN": 'sandbox501c49d7fb8740899eb08186177ba399.mailgun.org'
+        "MAILGUN_SENDER_DOMAIN": os.getenv('MAILGUN_DOMAIN')
     }
