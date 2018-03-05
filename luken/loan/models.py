75324ab6d6ee5c0cb29f6c8efa0d625802a33220
Dmitry Shlagoff <wessmoke@mail.ru>
2018-03-05 10:45:10 +0700

    Added email templates + model fixes

--- luken/loan/models.py
+++ luken/loan/models.py
@@ -1,6 +1,14 @@
 from django.conf import settings
-from django.core.mail import send_mail
+from django.core.mail import EmailMultiAlternatives
 from django.db import models
+from django.template import (
+    loader,
+    Context,
+)
+
+
+plaintext = loader.get_template("loan/email.txt")
+html = loader.get_template("loan/email.html")
 
 
 class LoanApplication(models.Model):
@@ -17,10 +25,10 @@ class LoanApplication(models.Model):
     )
 
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT, null=True)
-    email = models.EmailField()
-    loaned_amount = models.DecimalField(decimal_places=10, max_digits=20)
-    bitcoin_collateral = models.DecimalField(decimal_places=10, max_digits=20)
-    bitcoin_price_usd = models.DecimalField(decimal_places=10, max_digits=20)
+    email = models.EmailField(blank=True)
+    loaned_amount = models.DecimalField(decimal_places=2, max_digits=20)
+    bitcoin_collateral = models.DecimalField(decimal_places=8, max_digits=20)
+    bitcoin_price_usd = models.DecimalField(decimal_places=2, max_digits=20)
     terms_month = models.SmallIntegerField(choices=TERMS_MONTH_CHOICES)
     state = models.SmallIntegerField(choices=STATE_CHOICES)
 
@@ -29,15 +37,22 @@ class LoanApplication(models.Model):
 
     @classmethod
     def send_email(cls, sender, instance, created, **kwargs):
-        if not instance.email:
+        if not instance.email or not created:
             return
 
-        send_mail(
+        ctx = Context({"loan": instance})
+
+        text_content = plaintext.render(ctx)
+        html_content = html.render(ctx)
+
+        msg = EmailMultiAlternatives(
             "Mail about Loan Application",
-            "Some useful text about Loan Application",
+            text_content,
             "Loan Application mail sender (should be in settings, I guess)",
             [instance.email],
         )
+        msg.attach_alternative(html_content, "text/html")
+        msg.send()
 
 
 models.signals.post_save.connect(LoanApplication.send_email, sender=LoanApplication)
